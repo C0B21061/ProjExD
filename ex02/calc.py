419839a4330858345f966130d77d01f58afed7a6
shoma <c0b210619f@edu.teu.ac.jp>
2022-10-04 15:32:27 +0900

    7番まで終わった

--- ex02/calc.py
+++ ex02/calc.py
@@ -2,11 +2,15 @@ from cgitb import text
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-def button_click(event):
+def num_click(event):
     btn = event.widget
     txt = btn["text"]
-    #tkm.showinfo(txt,f"{txt}のボタンがクリックされました")
     entry.insert(tk.END,txt)
+
+def eq_click(event):
+    ans = eval(entry.get())
+    entry.delete(0,tk.END)
+    entry.insert(tk.END,ans)
 
 root = tk.Tk()
 root.title("電卓")
@@ -17,17 +21,17 @@ entry.grid(row=0,column=0,columnspan=3)
 
 for i in range(9,-1,-1):
     button = tk.Button(root,text=f"{i}",font=("times New Roman",30),width=4,height=2)
-    button.bind("<1>",button_click)
+    button.bind("<1>",num_click)
     r = [4,3,3,3,2,2,2,1,1,1]
     c = [0,2,1,0,2,1,0,2,1,0]
     button.grid(row=r[i],column=c[i])
 
 button = tk.Button(root,text=f"+",font=("times New Roman",30),width=4,height=2)
-button.bind("<1>",button_click)
+button.bind("<1>",num_click)
 button.grid(row=4,column=1)
 
 button = tk.Button(root,text=f"=",font=("times New Roman",30),width=4,height=2)
-button.bind("<1>",button_click)
+button.bind("<1>",eq_click)
 button.grid(row=4,column=2)
 
 root.mainloop()
