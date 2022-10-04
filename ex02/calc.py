34c18fbabf5fe25643bbdfd3f922e0685c31fb23
shoma <c0b210619f@edu.teu.ac.jp>
2022-10-04 15:02:17 +0900

    6番まで終わった

--- ex02/calc.py
+++ ex02/calc.py
@@ -22,4 +22,12 @@ for i in range(9,-1,-1):
     c = [0,2,1,0,2,1,0,2,1,0]
     button.grid(row=r[i],column=c[i])
 
+button = tk.Button(root,text=f"+",font=("times New Roman",30),width=4,height=2)
+button.bind("<1>",button_click)
+button.grid(row=4,column=1)
+
+button = tk.Button(root,text=f"=",font=("times New Roman",30),width=4,height=2)
+button.bind("<1>",button_click)
+button.grid(row=4,column=2)
+
 root.mainloop()
