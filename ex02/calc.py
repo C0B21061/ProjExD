938504dda08bb7044b9001e00c58a1f1a6891b8d
shoma <c0b210619f@edu.teu.ac.jp>
2022-10-04 17:06:42 +0900

    改良版

--- ex02/calc.py
+++ ex02/calc.py
@@ -4,6 +4,14 @@ from math import log10
 
 enzan_list=["**","+","-","*","/"]
 
+def color1(event):
+    event.widget["bg"]="#000000"
+    event.widget["fg"]="#ffffff"
+
+def color2(event):
+    event.widget["bg"]="SystemButtonFace"
+    event.widget["fg"]="#000000"
+
 def num_click(event):
     btn = event.widget
     txt = btn["text"]
@@ -41,33 +49,47 @@ entry.grid(row=0,column=0,columnspan=3)
 for i in range(9,-1,-1):
     button = tk.Button(root,text=f"{i}",font=("times New Roman",30),width=4,height=2)
     button.bind("<1>",num_click)
+    button.bind("<Enter>",color1)
+    button.bind("<Leave>",color2)
     r = [5,4,4,4,3,3,3,2,2,2]
     c = [1,2,1,0,2,1,0,2,1,0]
     button.grid(row=r[i],column=c[i])
 
 button = tk.Button(root,text="AC",font=("times New Roman",30),width=4,height=2)
 button.bind("<1>",AC_click)
+button.bind("<Enter>",color1)
+button.bind("<Leave>",color2)
 button.grid(row=1,column=0)
 
 button = tk.Button(root,text="C",font=("times New Roman",30),width=4,height=2)
 button.bind("<1>",C_click)
+button.bind("<Enter>",color1)
+button.bind("<Leave>",color2)
 button.grid(row=1,column=1)
 
 button = tk.Button(root,text="=",font=("times New Roman",30),width=4,height=2)
 button.bind("<1>",eq_click)
+button.bind("<Enter>",color1)
+button.bind("<Leave>",color2)
 button.grid(row=5,column=2)
 
 for i,enzan in enumerate(enzan_list,1):
     button = tk.Button(root,text=f"{enzan}",font=("times New Roman",30),width=4,height=2)
     button.bind("<1>",num_click)
+    button.bind("<Enter>",color1)
+    button.bind("<Leave>",color2)
     button.grid(row=i,column=4)
 
 button = tk.Button(root,text="log10",font=("times New Roman",30),width=4,height=2)
 button.bind("<1>",log_click)
+button.bind("<Enter>",color1)
+button.bind("<Leave>",color2)
 button.grid(row=1,column=2)
 
 button = tk.Button(root,text="bin",font=("times New Roman",30),width=4,height=2)
 button.bind("<1>",bin_click)
+button.bind("<Enter>",color1)
+button.bind("<Leave>",color2)
 button.grid(row=5,column=0)
 
 root.mainloop()
