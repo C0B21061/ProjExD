4524cb699080cdb07c8e49484f8242235702cde2
shoma <c0b210619f@edu.teu.ac.jp>
2022-10-18 17:00:01 +0900

    改良版

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.messagebox as tkm
 import maze_maker
 
 def key_down(event):
@@ -10,7 +11,7 @@ def key_up(event):
     key = ""
 
 def main_proc():
-    global cx,cy,mx,my
+    global cx,cy,mx,my,ch_mx,ch_my
     if key == "Up":
         my -= 1
     elif key == "Down":
@@ -20,6 +21,8 @@ def main_proc():
     elif key == "Right":
         mx += 1
     if maze[my][mx] == 0:
+        if mx==1 and my==7:
+            mx, my = 13,1
         cx, cy = mx*100+50, my*100+50
     else:
         if key == "Up":
@@ -31,7 +34,36 @@ def main_proc():
         elif key == "Right":
             mx -= 1
     canv.coords("tori",cx,cy)
-    root.after(100,main_proc)
+    
+    if key == "Up":
+        ch_my = my + 1
+        ch_mx = mx
+        canv.create_rectangle(ch_mx*100, ch_my*100, ch_mx*100+100, ch_my*100+100, 
+                                    fill="gray")
+        maze[ch_my][ch_mx]=1
+    elif key == "Down":
+        ch_my = my - 1
+        ch_mx = mx
+        canv.create_rectangle(ch_mx*100, ch_my*100, ch_mx*100+100, ch_my*100+100, 
+                                    fill="gray")
+        maze[ch_my][ch_mx]=1
+    elif key == "Left":
+        ch_mx = mx + 1
+        ch_my = my
+        canv.create_rectangle(ch_mx*100, ch_my*100, ch_mx*100+100, ch_my*100+100, 
+                                    fill="gray")
+        maze[ch_my][ch_mx]=1
+    elif key == "Right":
+        ch_mx = mx - 1
+        ch_my = my
+        canv.create_rectangle(ch_mx*100, ch_my*100, ch_mx*100+100, ch_my*100+100, 
+                                    fill="gray")
+        maze[ch_my][ch_mx]=1
+
+    if mx==13 and my==7:
+        tkm.showinfo("おめでとう！","ゴールしました！")
+    else:
+        root.after(100,main_proc)
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -45,6 +77,7 @@ if __name__ == "__main__":
 
     cx,cy = 300,400
     mx,my = 1,1
+    ch_mx,ch_my=1,1
     canv.create_image(cx,cy,image=tori,tag="tori")
     canv.pack()
 
